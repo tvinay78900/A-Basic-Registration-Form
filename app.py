901fbf7a63c446f3c1c4f60f67aa80b7d4a9355a
Vinay Tiwari <tvinay78900@gmail.com>
2026-06-28 15:44:24 +0530

    Update Environment Variable

--- app.py
+++ app.py
@@ -4,34 +4,49 @@ import os
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
+# Database connection function
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
 
-cursor = db.cursor()
-
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# Register route
 @app.route("/register", methods=["POST"])
 def register():
+
+    # Form data
     name = request.form["name"]
     email = request.form["email"]
     phone = request.form["phone"]
 
+    # New DB connection
+    db = get_db_connection()
+    cursor = db.cursor()
+
+    # Insert query
     cursor.execute(
-        "INSERT INTO users(name,email,phone) VALUES (%s,%s,%s)",
+        "INSERT INTO users(name, email, phone) VALUES(%s, %s, %s)",
         (name, email, phone)
     )
 
+    # Save changes
     db.commit()
 
-    return "Registration Successful ✔"
+    # Close connection
+    cursor.close()
+    db.close()
 
+    return "Registration Successful"
+
+# Run app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
